c4c8f35303fc2f880b4cb9f3d99bfc63aa22a45f
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2022-01-18 22:40:28 +0100

    connect bienvenue

--- src/components/Bienvenue.tsx
+++ src/components/Bienvenue.tsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { getCurrentUser } from "../services/auth.service";
 
 const Bienvenue: React.FC = () => {
+
+  const UserOrNot = getCurrentUser()
+
+  useEffect(() => {
+    
+    
+  }, []);
+  
 
   return (
     <div style={{ height: "60vh", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <h1>Bienvenue sur l'application Homners</h1>
       <div className="lienAnnonce" style={{ marginTop: "2em" }}>
+        {UserOrNot == undefined ? 
+        <Link to={"/login"}>
+          <h2 >Connectez-vous 🚀</h2>
+        </Link>  
+      : 
         <Link to={"/espace"}>
           <h2 >Accéder à votre espace 🚀</h2>
-        </Link>  
+        </Link> 
+      }
+        
       </div>
     </div>
   );
